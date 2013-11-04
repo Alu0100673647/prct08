604b145a50f0248a5a776bc713c6dd6cb4182dad
constanza León <alu0100673647@ull.edu.es>
2013-11-04 20:01:19 +0000

    Añadiendo ficheros necesarios para la realización de la práctica8, al repositorio

---
+++ lib/matriz.rb
@@ -0,0 +1,75 @@
+# Practica 8 Lenguajes y Paradigmas de la programación usando #desarrollo dirigido por pruebas (Test Driven Development - TDD) con #la herramienta Rspec.
+# Pedro Javier Núñez Rodríguez
+# Constanza Leon Baritussio
+# 
+
+class Matriz
+  include Comparable
+
+  attr_reader :n,:m,:fa, :fb, :fc, :fd, :A, :B, :C, :D
+
+  def initialize(n,m,fa,fb,fc,fd,A,B,C,D)
+
+   @n = n
+   @m = m
+   @fa = []
+   @fb = []
+   @fc = []
+   @fd = []
+   @A = []
+   @B = []
+   @C = []
+   @D = []
+  end
+
+  def tam_matriz(n,m)
+	print ' introduzca las filas de las matrices: '
+	n = gets.chomp.to_i
+
+	print ' introduzca las columnas de la matrices: '
+	m = gets.chomp.to_i
+
+	for i in 0...n
+	for j in 0...m
+		fb[j] = rand(100)
+		fa[j] = rand(100)
+		fc[j] = 0
+		fd[j] = 0
+	end
+	A << fa;
+	B << fb;
+	C << fc;
+	D << fd;
+
+   end
+  def to_s
+  end
+
+  def to_i
+  end	
+
+  def matriz_por_un_escalar
+  end
+
+  def opuesto_matriz
+  end
+
+  def multiplicar
+  end
+
+  def sumar
+  end
+
+  def dividir
+  end
+
+  def restar
+  end
+
+  def <=>
+  end
+  
+  
+  
+
+end
---
+++ spec/matriz_spec.rb
@@ -0,0 +1,63 @@
+# Fichero con las espectativas de la clase Matriz
+# Cualquier cambio hace que se ejecute guard
+
+require 'matriz'
+
+describe "Matriz" do
+  before :each do
+    
+  end    
+    
+ 
+    it "Se almacena correctamente la matriz 1" do
+   
+    end
+    it "Se almacena correctamente la matriz 2" do
+        
+  end
+  
+
+   it "la matriz esta en formato to_s"do
+  end
+   
+   it "la matriz esta en formato to_i"do
+  end
+   
+  end
+  
+ 
+    it "Se multiplica un numero por todos los elementos de la matriz" do
+      
+    end
+    it "Si se multiplica por -1 se obtiene el opuesto de la matriz" do
+      
+  end
+  
+   it "Se multiplican la matriz 1 por la matriz 2" do
+      
+  end
+  
+   it "Se suman las matrices 1 y 2" do
+      
+    end
+   it "Se dividen las matrices 1 y 2" do
+    
+    end
+ 
+    it "Se restan las matrices 1 y 2" do
+      
+    end
+    it "la matriz 1 es menor o igual que la matriz2" do
+     
+    end
+    it "la matriz 1 es mayor que la matriz2" do
+     
+    end
+    it "la matriz 1 es mayor o igual que la matriz2 (1,1)" do
+      
+    end
+    it "la matriz 1 es menor que la matriz2" do
+      
+    end
+  end
+end
